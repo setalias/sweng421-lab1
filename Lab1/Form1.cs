6cf2aa955ca58c86e51469c74a4447b6dde14bb9
setalias <sandy@sandylee.me>
2021-08-25 20:18:32 -0500

    Add project files.

---
+++ Lab1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void trackBar1_ValueChanged(object sender, EventArgs e)
+		{
+			Console.WriteLine(trackBar1.Value);
+		}
+
+		int oldX, oldY;
+		private void panel1_MouseDown(object sender, MouseEventArgs e)
+		{
+			oldX = e.X;
+			oldY = e.Y;
+			
+		}
+
+		private void panel1_MouseUp(object sender, MouseEventArgs e)
+		{
+			Pen pen = new Pen(Color.Red);
+			Graphics g = panel1.CreateGraphics();
+			g.DrawLine(pen, oldX, oldY, e.X, e.Y);
+		}
+
+		private void panel1_MouseMove(object sender, MouseEventArgs e)
+		{
+
+		}
+	}
+}
---
+++ Lab1/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+	public class Shape
+	{
+		int oldx, oldy, newx, newy, w, h;
+		Color color;
+
+		public void drawShape(Panel p, Color c) { }
+		//public void bufferShape(Graphics fgg) { } 
+	}
+}
