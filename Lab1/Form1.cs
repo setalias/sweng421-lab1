f089d57f408e0f7880125b2a71dad68a80213d1a
setalias <sandy@sandylee.me>
2021-08-28 22:44:14 -0500

    Double Buffering working Rectangle not going to negative values

--- Lab1/Form1.cs
+++ Lab1/Form1.cs
@@ -23,24 +23,31 @@ namespace Lab1
 		int green;
 		int blue;
 
+		int start_x;
+		int start_y;
+
 		Bitmap bg, fg;
 		Graphics bgg, fgg;
 		Point pt1, pt2;
 		bool down = false;
+		String shape_type;
 		
 		private void button1_Click(object sender, EventArgs e)
 		{
 			s = new Line();
+			shape_type = "line_shape";
 		}
 
 		private void button2_Click(object sender, EventArgs e)
 		{
 			s = new Rectangle();
+			shape_type = "rectangle_shape";
 		}
 
 		private void button3_Click(object sender, EventArgs e)
 		{
 			s = new Ellipse();
+			shape_type = "ellipse_shape";
 		}
 
 		private void trackBar1_ValueChanged(object sender, EventArgs e)
@@ -60,21 +67,25 @@ namespace Lab1
 
 		private void panel1_MouseDown(object sender, MouseEventArgs e)
 		{
-			s.oldx = e.X;
-			s.oldy = e.Y;
+			//s.oldx = e.X;
+			//s.oldy = e.Y;
 
+			start_x = e.X;
+			start_y = e.Y;
 
 			down = true;
-			if(bg == null)
+			pt1 = e.Location;
+
+			if (bg == null)
             {
-				bg = new Bitmap(this.Width, this.Height);
-				fg = new Bitmap(this.Width, this.Height);
+				bg = new Bitmap(panel1.Width, panel1.Height);
+				fg = new Bitmap(panel1.Width, panel1.Height);
 				bgg = Graphics.FromImage(bg);
-				bgg.FillRectangle(Brushes.White, 0, 0, this.Width, this.Height);
+				bgg.FillRectangle(Brushes.White, 0, 0, panel1.Width, panel1.Height);
 				fgg = Graphics.FromImage(fg);
             }
 
-			pt1 = e.Location;
+			
 			
 		}
 
@@ -82,30 +93,43 @@ namespace Lab1
 		{
 			down = false;
 			bgg.DrawImage(fg, 0, 0);
-
+			
 			//Pen pen = new Pen(Color.Red);
 			//Graphics g = panel1.CreateGraphics();
 			//g.DrawLine(pen, oldX, oldY, e.X, e.Y);
-			Color c = new Color();
-			c = Color.FromArgb(red,green,blue);
-			s.newx = e.X;
-			s.newy = e.Y;
-			s.drawColoredShape(panel1,c);
+			//Color c = new Color();
+			//c = Color.FromArgb(red,green,blue);
+			//s.newx = e.X;
+			//s.newy = e.Y;
+			//s.drawColoredShape(panel1,c);
+	
 		}
 
 		private void panel1_MouseMove(object sender, MouseEventArgs e)
 		{
 			if (down)
             {
+
+				Color c = new Color();
+				c = Color.FromArgb(red, green, blue);
 				fgg.DrawImage(bg, 0, 0);
-				Pen pen = new Pen(Color.Red);
+				Pen pen = new Pen(c);
 				pt2 = e.Location;
-				fgg.DrawLine(pen, pt1, pt2);
-				Graphics g = this.CreateGraphics();
+				if (shape_type == "line_shape") {
+					fgg.DrawLine(pen, pt1, pt2);
+				}
+				else if (shape_type == "rectangle_shape")
+				{
+					fgg.DrawRectangle(pen, new System.Drawing.Rectangle(start_x, start_y, e.X - start_x, e.Y - start_y));
+				}
+				else if (shape_type == "ellipse_shape")
+				{
+					fgg.DrawEllipse(pen, new System.Drawing.Rectangle(start_x, start_y, e.X - start_x, e.Y - start_y));
+				}
+				Graphics g = panel1.CreateGraphics();
 				g.DrawImage(fg, 0, 0);
+
             }
 		}
-
-        
     }
 }
