12422eccff0b87d90cbf7b6ba568dd743dc19186
Darlene <darlene.in93@gmail.com>
2021-08-29 01:07:01 -0400

    New chages

--- Lab1/Form1.cs
+++ Lab1/Form1.cs
@@ -1,81 +1,102 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lab1
 {
 	public partial class Form1 : Form
 	{
-		Shape s = new Shape();
-		int red;
-		int green;
-		int blue;
 
 		public Form1()
 		{
 			InitializeComponent();
 		}
 
+		Shape s = new Shape();  // to get shape 
+		int red, blue, green;   // for rbg pen color 
+		Bitmap bg, fg;
+		Graphics bgg, fgg;
+		bool down = false;
+
 		private void button1_Click(object sender, EventArgs e)
 		{
-			s = new Line();
+			s = new Line(); // line button
 		}
 
 		private void button2_Click(object sender, EventArgs e)
 		{
-			s = new Rectangle();
+			s = new Rectangle(); // rectangle button 
 		}
 
 		private void button3_Click(object sender, EventArgs e)
 		{
-			s = new Ellipse();
+			s = new Ellipse(); // ellipse button 
 		}
 
 		private void trackBar1_ValueChanged(object sender, EventArgs e)
 		{
-			red = trackBar1.Value;
+			red = trackBar1.Value; // red tracker
 		}
 
 		private void trackBar2_ValueChanged(object sender, EventArgs e)
 		{
-			green = trackBar2.Value;
+			green = trackBar2.Value; // green tracker 
 		}
 
 		private void trackBar3_ValueChanged(object sender, EventArgs e)
 		{
-			blue = trackBar3.Value;
+			blue = trackBar3.Value; // blue tracker 
 		}
 
 		private void panel1_MouseDown(object sender, MouseEventArgs e)
 		{
-			s.oldx = e.X;
-			s.oldy = e.Y;
-			
+			// get starting values of shape 
+			s.start_x = e.X;
+			s.start_y = e.Y;
+
+			// mouse is down
+			down = true;
+
+			// if background null, create background and foreground 
+			if (bg == null)
+			{
+				bg = new Bitmap(panel1.Width, panel1.Height);
+				fg = new Bitmap(panel1.Width, panel1.Height);
+				bgg = Graphics.FromImage(bg);
+				bgg.FillRectangle(Brushes.White, 0, 0, panel1.Width, panel1.Height);
+				fgg = Graphics.FromImage(fg);
+			}
 		}
 
 		private void panel1_MouseUp(object sender, MouseEventArgs e)
 		{
-			//Pen pen = new Pen(Color.Red);
-			//Graphics g = panel1.CreateGraphics();
-			//g.DrawLine(pen, oldX, oldY, e.X, e.Y);
-			Color c = new Color();
-			c = Color.FromArgb(red,green,blue);
-			s.newx = e.X;
-			s.newy = e.Y;
-			s.drawColoredShape(panel1,c);
+
+			down = false;               // mouse is no longer down
+			bgg.DrawImage(fg, 0, 0);    // keep image on panel before new image
 		}
 
 		private void panel1_MouseMove(object sender, MouseEventArgs e)
 		{
+			if (down)
+			{
+				fgg.DrawImage(bg, 0, 0);
 
+				// get color from sliders 
+				Color c = new Color();
+				c = Color.FromArgb(red, green, blue);
+
+				// get end location for shape 
+				s.end_x = e.X;
+				s.end_y = e.Y;
+
+				// draw the colored shape 
+				s.drawColoredShape(fgg, c);
+
+				// draw to panel 
+				Graphics g = panel1.CreateGraphics();
+				g.DrawImage(fg, 0, 0);
+
+			}
 		}
-
-        
-    }
+	}
 }
--- Lab1/Rectangle.cs
+++ Lab1/Rectangle.cs
@@ -1,30 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Lab1
 {
     public class Rectangle : Shape
     {
-        public override void drawColoredShape(Panel p, Color c)
+        public override void drawColoredShape(Graphics fgg, Color c)
         {
             Pen pen = new Pen(c);
-            Graphics g = p.CreateGraphics();
 
-            w = Math.Abs(newx - oldx); // width
-            h = Math.Abs(newy - oldy); // height
-            int diffx = newx - oldx; // direction of cursor drag for x-axis
-            int diffy = newy - oldy; // direction of cursor drag for y-axis
+            w = Math.Abs(end_x - start_x); // width
+            h = Math.Abs(end_y - start_y); // height
+            int diffx = end_x - start_x; // direction of cursor drag for x-axis
+            int diffy = end_y - start_y; // direction of cursor drag for y-axis
 
             // draw rectangle
-            if (diffx > 0 && diffy < 0) g.DrawRectangle(pen, oldx, newy, w, h);
-            else if (diffx < 0 && diffy > 0) g.DrawRectangle(pen, newx, oldy, w, h);
-            else if (diffx < 0 && diffy < 0) g.DrawRectangle(pen, newx, newy, w, h);
-            else if (diffx > 0 && diffy > 0) g.DrawRectangle(pen, oldx, oldy, w, h);
+            if (diffx > 0 && diffy < 0) fgg.DrawRectangle(pen, start_x, end_y, w, h);
+            else if (diffx < 0 && diffy > 0) fgg.DrawRectangle(pen, end_x, start_y, w, h);
+            else if (diffx < 0 && diffy < 0) fgg.DrawRectangle(pen, end_x, end_y, w, h);
+            else if (diffx > 0 && diffy > 0) fgg.DrawRectangle(pen, start_x, start_y, w, h);
         }
     }
 }
